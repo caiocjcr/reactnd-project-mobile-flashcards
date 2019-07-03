2543a9a779d4cf39ab27cc48685e828da789b551
Caio Rosa <caio.cjcr@gmail.com>
2019-07-03 18:33:06 -0300

    Implement base navigation

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation'
+import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const Tabs = createBottomTabNavigator({
+  Decks: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Your decks',
+      tabBarIcon: ({tintColor}) => <MaterialCommunityIcons name='cards' size={30} style={{ color: tintColor }}/>
+    }
   },
-});
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New deck',
+      tabBarIcon: ({tintColor}) => <Ionicons name='ios-create' size={30} style={{ color: tintColor }}/>
+    }
+  }
+})
+
+const MainStackNavigation = createStackNavigator({
+  Home: {
+    screen: Tabs,
+    navigationOptions: {
+      header: null
+    }
+  }
+})
+
+const MainNavigation = createAppContainer(MainStackNavigation)
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <MainNavigation />
+    )
+  }
+}
