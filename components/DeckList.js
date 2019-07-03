6e63895efc0ef06be79cd2731dabb266803b9228
Caio Rosa <caio.cjcr@gmail.com>
2019-07-03 19:10:13 -0300

    Receive decks from AsyncStorage and list their IDs on decklist

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { connect } from 'react-redux' 
+import { getAllDecks } from '../utils/API';
+import { receiveDecks } from '../actions';
 
-const DeckList = () => {
-    return (
-        <View>
-            <Text>Deck list</Text>
-        </View>
-    )
+class DeckList extends Component {
+    componentDidMount() {
+        getAllDecks()
+            .then(res => this.props.dispatch(receiveDecks(res)))
+    }
+    render() {
+        return (
+            <View>
+                {this.props.deckIds.map(id => (
+                    <View key={id}>
+                        <Text>{id}</Text>
+                    </View>
+                ))}
+            </View>
+        )
+    }
 }
 
-export default DeckList
+function mapStateToProps(state) {
+    return {
+        deckIds: Object.keys(state.decks)
+    }
+}
+
+export default connect(mapStateToProps)(DeckList)
